e69ed04c6c71008ab659a74d682f8633903ab058
Michael Clausen <encodeering@gmail.com>
2018-01-03 13:30:33 +0100

    copies skeleton project with properties
    * common environment variables
    * ci package
    * git pinning
    
    NOTE
    
    * commit won't succeed, build definition is missing

---
+++ modules/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# configuration
+#   env.ARCH
+#   env.PROJECT
+#   env.VERSION
+#   env.REPOSITORY
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
