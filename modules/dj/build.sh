e46b052da8cbeac0bfc380dcdae0a8ef73b9f521
Michael Clausen <encodeering@gmail.com>
2018-01-04 19:11:50 +0100

    limits the number of concurrent go builds
    * build becomes stuck and spawned go build processes are not claimed - used docker exec -it .. to inspect the running processes
    
    NOTE
    
    * go install (keeps previously build artefacts) with timeout helper could be an option otherwise: timeout -t 60 -s 9 go install -v -p 1 .
    
    * https://golang.org/cmd/go/
    * https://stackoverflow.com/questions/5274294/how-can-you-run-a-command-in-bash-over-until-success

--- modules/dj/build.sh
+++ modules/dj/build.sh
@@ -13,5 +13,6 @@ import com.encodeering.docker.docker
 docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.7"
 
 patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < patch/Dockerfile.patch
+patch -p1 --no-backup-if-mismatch --directory="$PROJECT" < patch/Makefile.patch
 
 docker build -t "$DOCKER_IMAGE" "$PROJECT"
